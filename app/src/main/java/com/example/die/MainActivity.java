74d193f2af9f109f578a54d09eed00e34119484c
natto132001 <107522649+natto132001@users.noreply.github.com>
2022-06-29 14:14:16 +0900

    Main

--- app/src/main/java/com/example/die/MainActivity.java
+++ app/src/main/java/com/example/die/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import natto132001.java;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener((View v) -> {
+            startActivity(new Intent(this, SubActivity.class));
+        });
+
     }
 }
