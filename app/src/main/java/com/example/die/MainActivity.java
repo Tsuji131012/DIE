bdee291994879b66e13ccd4c73f47a11a0e935de
natto132001 <107522649+natto132001@users.noreply.github.com>
2022-06-29 23:07:14 +0900

    どのボタンからでもChoiceActivityに飛べるか検証してください。

--- app/src/main/java/com/example/die/MainActivity.java
+++ app/src/main/java/com/example/die/MainActivity.java
@@ -8,13 +8,17 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
+finish int DISHIES = 20;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        /*
         Button button1 = findViewById(R.id.button);
 
         Button button1 = findViewById(R.id.button23);
@@ -26,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+         */
+
+        //実験
+
+
+
+        Button dishies[] = new Button[DISHIES];
+
+        for (int i = 0;i < DISHIES;i++){
+            dishies[i] = findViewById(R.id.button[i]);
+
+            dishies[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplication(), ChoiceActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
 
         //tttttt
 
