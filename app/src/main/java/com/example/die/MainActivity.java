3483b9c47d0591d40140fd68311eebf7481afbd7
Tsuji131012 <107450037+Tsuji131012@users.noreply.github.com>
2022-07-02 16:29:16 +0900

    どのボタンでも画面遷移できるプログラムの修正と 料理・材料確認画面のインターフェース作成

--- app/src/main/java/com/example/die/MainActivity.java
+++ app/src/main/java/com/example/die/MainActivity.java
@@ -8,16 +8,13 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-finish int DISHIES = 20;
-
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         /*
         Button button1 = findViewById(R.id.button);
 
@@ -35,22 +32,21 @@ public class MainActivity extends AppCompatActivity {
         //実験
 
 
+        View.OnClickListener event = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), ChoiceActivity.class);
+                startActivity(intent);
+            }
+        };
 
-        Button dishies[] = new Button[DISHIES];
-
-        for (int i = 0;i < DISHIES;i++){
-            dishies[i] = findViewById(R.id.button[i]);
-
-            dishies[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getApplication(), ChoiceActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
-
-        //tttttt
+        findViewById(R.id.button0).setOnClickListener(event);
+        findViewById(R.id.button1).setOnClickListener(event);
+        findViewById(R.id.button2).setOnClickListener(event);
+        findViewById(R.id.button3).setOnClickListener(event);
+        findViewById(R.id.button4).setOnClickListener(event);
+        findViewById(R.id.button5).setOnClickListener(event);
+        findViewById(R.id.button6).setOnClickListener(event);
 
     }
 }
