a75a55080b98886fd1d857285fce38304ef1f0cd
Tsuji131012 <107450037+Tsuji131012@users.noreply.github.com>
2022-07-18 16:29:25 +0900

    カード選択画面のインターフェース完成

--- app/src/main/java/com/example/die/CardActivity.java
+++ app/src/main/java/com/example/die/CardActivity.java
@@ -11,4 +11,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+
+
+    }
 }
